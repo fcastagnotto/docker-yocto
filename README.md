7238c34e9caddfe8786549baf01335e6eb66cfd5
F.Castagnotto <fcastagnotto@linux.com>
2018-03-29 13:41:35 +0200

    Fix missing LANG configuration. Creation of full virtual image, instead of minimal.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 First try of build os yocto-based using docker image
 
 [2018.03.29] File "setup_env.sh" start creation of environment into a docker container (ubuntu 14.04) and create the Qemu Virtual Image
+[2018.03.29] Fix missing LANG configuration. Creation of full virtual image, instead 
+of minimal.
--- make_yocto_base.sh
+++ make_yocto_base.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+cd poky
 source oe-init-build-env
 echo ""
 echo "Starting creation of minimal Yocto image..."
 echo "--------------------------------------------"
 echo ""
-bitbake -k core-image-minimal
+bitbake -k core-image-full-cmdline
